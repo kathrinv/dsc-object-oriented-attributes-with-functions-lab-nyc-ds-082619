0af9f357e8779d2c507bb147e61fd54e6fb0e9b5
kathrin v <go.kathrin@gmail.com>
2019-09-05 16:20:04 -0400

    added sort_roster method

--- school.py
+++ school.py
@@ -12,3 +12,15 @@ class School():
             self.roster[grade_level].append(full_name)
         else:
             self.roster[grade_level] = [full_name]
+            
+    def grade(self, grade_num):
+        if grade_num in self.roster:
+            return self.roster[grade_num]
+        else:
+            print("No data for this grade")
+    
+    def sort_roster(self):
+        self.roster = {x:sorted(self.roster[x]) for x in self.roster.keys()}
+        #for grade in self.roster:
+        #    self.roster[grade] = sorted(self.roster[grade])
+        return self.roster
