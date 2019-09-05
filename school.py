53708218608963aec5e952aa78662442a9eccc48
kathrin v <go.kathrin@gmail.com>
2019-09-05 16:17:53 -0400

    fixed add_student method, added sort_roster method

--- school.py
+++ school.py
@@ -1,14 +1,14 @@
 class School():
 
-    def __init__(self, name=None, roster={}):
+    def __init__(self, name=None, roster=None):
         self.name = name
-        self.roster = roster
+        if roster is None:
+            self.roster = {}
+        else:
+            self.roster = roster
 
     def add_student(self, full_name, grade_level):
         if grade_level in self.roster:
-            self.roster[grade_level] = self.roster[grade_level].append(full_name)
-            print(self.roster)
-	else:
-        self.roster[grade_level] = [].append(full_name)
-	    print(self.roster)
-	    print(self.roster[grade_level])
+            self.roster[grade_level].append(full_name)
+        else:
+            self.roster[grade_level] = [full_name]
