0db99ddc936c97075ce1896261178c4682536213
kathrin v <go.kathrin@gmail.com>
2019-09-05 10:44:40 -0400

    added init and add_student methods to the School class

--- school.py
+++ school.py
@@ -1,1 +1,11 @@
 class School:
+
+    def __init__(self, name=None, roster={}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, full_name, grade_level):
+        if grade_level in self.roster:
+            self.roster[grade_level] = self.roster[grade_level].append(full_name)
+        else:
+            self.roster[grade_level] = [full_name]
